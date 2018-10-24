7a450108b332a0e034db0308996ec02f36cc8390
unknown <nubang7@gmail.com>
2018-10-25 01:22:29 +0900

    first commit

---
+++ src/mokito_trainning/MockitoTrainningTest.java
@@ -0,0 +1,16 @@
+package mokito_trainning;
+
+import org.junit.Test;
+
+public class MockitoTrainningTest {
+
+	@Test
+	public void mockito_List_테스트() {
+		
+	}
+	
+	@Test
+	public void mockito_LinkedList_테스트() {
+		
+	}
+}
