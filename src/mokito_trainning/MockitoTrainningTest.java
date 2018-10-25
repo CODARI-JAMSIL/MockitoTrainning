bc6bbfb46298ebaf20fe7f9cbaced5cf48d80868
nubang <nubang7@gmail.com>
2018-10-25 20:34:21 +0900

    밋업 강사 Push

--- src/mokito_trainning/MockitoTrainningTest.java
+++ src/mokito_trainning/MockitoTrainningTest.java
@@ -1,16 +1,34 @@
 package mokito_trainning;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class MockitoTrainningTest {
 
 	@Test
 	public void mockito_List_테스트() {
+		List mockedList= mock(List.class);
 		
+		mockedList.add("first");
+		mockedList.clear();
+		
+		verify(mockedList).add("first");
+		verify(mockedList).clear();
 	}
 	
 	@Test
 	public void mockito_LinkedList_테스트() {
+		LinkedList mockedList= mock(LinkedList.class);
 		
+		when(mockedList.get(0)).thenReturn("first");
+		
+		System.out.println(mockedList.get(0));
+		System.out.println(mockedList.get(999));
 	}
 }
